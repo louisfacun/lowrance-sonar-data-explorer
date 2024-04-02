fed0493e9ca0bb490f6b8a7a4510362261e609af
Louis Facun <louisfacun@gmail.com>
2024-04-02 21:22:52 +0800

    add unstable water column removal

--- lowmapper/sidescan.py
+++ lowmapper/sidescan.py
@@ -10,10 +10,82 @@ class SideScan:
         # The data is a 2d numpy array where each rows are pings and row's values
         # It is the intensity of the signal (or frames in 0-255 range).
         self.df = df
-        self.image = np.stack(self.df['frames'])
+        self.image = np.stack(self.df['frames']) #Sidescan image.
         self.config = config
 
 
+    def _wcr(self, port, starboard):
+        """Remove water column from the sidescan image.
+        
+        Args:
+            image (numpy.ndarray): Sidescan image.
+        
+        Returns:
+            numpy.ndarray: Sidescan image with water column removed.
+        """
+
+        # Get first row' as all values are same for all rows
+        absolute_range = self.df['max_range'].iloc[0]
+
+        if not all(self.df['max_range'] == absolute_range):
+            raise ValueError(
+                "Values in 'max_range' column are not consistent across all rows.")
+
+        # Assumes port and starboard has same size
+        # TODO: Check if same size
+        n_pings = port.shape[0] # number of pings in a session
+        n_frame = port.shape[1] # number of pixel in a frame
+
+        depths = self.df['water_depth']
+
+        # Horizontal distance of max range
+        horizontal_dist = np.sqrt(absolute_range**2 - depths**2).astype(int)
+
+        # Get equally spaced distances of horizontal
+        port_horizontal_dists = np.linspace(
+            horizontal_dist, depths, n_frame, axis=1).astype(int)
+        starboard_horizontal_dists = np.linspace(
+            depths, horizontal_dist, n_frame, axis=1).astype(int)
+        
+        # Convert to horizontal distances to pixel value
+        port_horizontal_dists = (
+            (port_horizontal_dists * n_frame) / absolute_range).astype(int)
+        starboard_horizontal_dists = (
+            (starboard_horizontal_dists * n_frame) / absolute_range).astype(int)
+
+        port_wcr = np.zeros((n_pings, n_frame))
+        starboard_wcr = np.zeros((n_pings, n_frame))
+
+        for n in range(n_pings):
+            port_frame = np.zeros((n_frame))
+            starboard_frame = np.zeros((n_frame))
+            for m in range(n_frame):
+                # Automatically removes water column with slant range correction
+                port_frame[m] = port[n][-port_horizontal_dists[n][m]]
+                starboard_frame[m] = starboard[n][starboard_horizontal_dists[n][m]]
+            port_wcr[n] = port_frame
+            starboard_wcr[n] = starboard_frame
+
+        return port_wcr, starboard_wcr
+    
+        # port_depths_px = ((depths * frame_px) / absolute_range).astype(int)
+        # starboard_depths_px = ((depths * frame_px) / absolute_range).astype(int)
+
+
+        # for i, (port_depth_px, starboard_depth_px) in enumerate(zip(port_depths_px, starboard_depths_px)):
+        #     port_wcr[i] = np.concatenate(
+        #         (
+        #             np.zeros(port_depth_px).astype(int), # black
+        #             port_wcr[i, :-port_depth_px]
+        #         )
+        #     )
+        #     starboard_wcr[i] = np.concatenate(
+        #         (
+        #             starboard_wcr[i, starboard_depth_px:], 
+        #             np.zeros((starboard_depth_px)).astype(int) # black
+        #         )
+        #     )
+            
     def _port(self):
         """Get port side of the sidescan image.
         
@@ -107,7 +179,8 @@ class SideScan:
 
         if self.config['water_column_removed']:
             print('Creating water column removed...')
-            # wcr_image = self._wcr(raw_image)
+            port_wcr, starboard_wcr = self._wcr(self._port(), self._starboard())
+            image = np.concatenate((port_wcr, starboard_wcr), axis=1)
             sonograms['sidescan_wcr'] = image
         
         for file_name, image in sonograms.items():
