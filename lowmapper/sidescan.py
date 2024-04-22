cb6c493d99f5830f441ec0e9815c7eeb2a2e6fd1
Louis Facun <louisfacun@gmail.com>
2024-04-22 13:21:35 +0800

    feat: add speed correction by interpolation

--- lowmapper/sidescan.py
+++ lowmapper/sidescan.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
 
 from .exporter import Exporter
 from .utils import x_to_longitude, y_to_latitude
+
 
 # Process designed for sidescan
 class SideScan:
@@ -10,48 +12,53 @@ class SideScan:
         # The data is a 2d numpy array where each rows are pings and row's values
         # It is the intensity of the signal (or frames in 0-255 range).
         self.df = df
-        self.image = np.stack(self.df['frames']) #Sidescan image.
+        self._add_cum_distance_travelled()
+        self.image = np.stack(self.df["frames"])  # Sidescan image.
         self.config = config
-
 
     def _wcr(self, port, starboard):
         """Remove water column from the sidescan image.
-        
+
         Args:
             image (numpy.ndarray): Sidescan image.
-        
+
         Returns:
             numpy.ndarray: Sidescan image with water column removed.
         """
 
         # Get first row' as all values are same for all rows
-        absolute_range = self.df['max_range'].iloc[0]
+        absolute_range = self.df["max_range"].iloc[0]
 
-        if not all(self.df['max_range'] == absolute_range):
+        if not all(self.df["max_range"] == absolute_range):
             raise ValueError(
-                "Values in 'max_range' column are not consistent across all rows.")
+                "Values in 'max_range' column are not consistent across all rows."
+            )
 
         # Assumes port and starboard has same size
         # TODO: Check if same size
-        n_pings = port.shape[0] # number of pings in a session
-        n_frame = port.shape[1] # number of pixel in a frame
+        n_pings = port.shape[0]  # number of pings in a session
+        n_frame = port.shape[1]  # number of pixel in a frame
 
-        depths = self.df['water_depth']
+        depths = self.df["water_depth"]
 
         # Horizontal distance of max range
         horizontal_dist = np.sqrt(absolute_range**2 - depths**2).astype(int)
 
         # Get equally spaced distances of horizontal
         port_horizontal_dists = np.linspace(
-            horizontal_dist, depths, n_frame, axis=1).astype(int)
+            horizontal_dist, depths, n_frame, axis=1
+        ).astype(int)
         starboard_horizontal_dists = np.linspace(
-            depths, horizontal_dist, n_frame, axis=1).astype(int)
-        
+            depths, horizontal_dist, n_frame, axis=1
+        ).astype(int)
+
         # Convert to horizontal distances to pixel value
         port_horizontal_dists = (
-            (port_horizontal_dists * n_frame) / absolute_range).astype(int)
+            (port_horizontal_dists * n_frame) / absolute_range
+        ).astype(int)
         starboard_horizontal_dists = (
-            (starboard_horizontal_dists * n_frame) / absolute_range).astype(int)
+            (starboard_horizontal_dists * n_frame) / absolute_range
+        ).astype(int)
 
         port_wcr = np.zeros((n_pings, n_frame))
         starboard_wcr = np.zeros((n_pings, n_frame))
@@ -67,10 +74,9 @@ class SideScan:
             starboard_wcr[n] = starboard_frame
 
         return port_wcr, starboard_wcr
-    
+
         # port_depths_px = ((depths * frame_px) / absolute_range).astype(int)
         # starboard_depths_px = ((depths * frame_px) / absolute_range).astype(int)
-
 
         # for i, (port_depth_px, starboard_depth_px) in enumerate(zip(port_depths_px, starboard_depths_px)):
         #     port_wcr[i] = np.concatenate(
@@ -81,29 +87,27 @@ class SideScan:
         #     )
         #     starboard_wcr[i] = np.concatenate(
         #         (
-        #             starboard_wcr[i, starboard_depth_px:], 
+        #             starboard_wcr[i, starboard_depth_px:],
         #             np.zeros((starboard_depth_px)).astype(int) # black
         #         )
         #     )
-            
+
     def _port(self):
         """Get port side of the sidescan image.
-        
+
         Returns:
             numpy.ndarray: Port side of the sidescan image.
         """
-        return self.image[:, :self.image.shape[1]//2]
-    
-    
+        return self.image[:, : self.image.shape[1] // 2]
+
     def _starboard(self):
         """Get starboard side of the sidescan image.
-        
+
         Returns:
             numpy.ndarray: Starboard side of the sidescan image.
         """
-        return self.image[:, self.image.shape[1]//2:]
-    
-    
+        return self.image[:, self.image.shape[1] // 2 :]
+
     def _egn(self, image):
         """Apply Empirical Gain Normalization to the sidescan image.
 
@@ -113,7 +117,7 @@ class SideScan:
         Returns:
             np.2d array
 
-        Note: 
+        Note:
             Normalized data by mean, not necessarily in the range [0, 1].
 
         Reference:
@@ -121,92 +125,104 @@ class SideScan:
         """
         mean_per_column = np.mean(image, axis=0)
         normalized_by_mean_image = image / mean_per_column
-        
+
         # Convert in the range [0, 1]
         min_value = np.min(normalized_by_mean_image)
         max_value = np.max(normalized_by_mean_image)
 
-        final = (normalized_by_mean_image-min_value) / (max_value-min_value)
-                
-        return (final * 255).astype(np.uint8)
+        final = (normalized_by_mean_image - min_value) / (max_value - min_value)
 
+        return (final * 255).astype(np.uint8)
 
     def _egn_min_max_stretch(self, image, min_value, max_value):
         """Apply min-max stretching to the sidescan EGN normalized image.
-        
+
         Returns:
             numpy.ndarray: Stretched data in the range [0, 1].
         """
-    
-        #_min = np.min(self.egn_data)
-        #_max = np.max(self.egn_data)
+
+        # _min = np.min(self.egn_data)
+        # _max = np.max(self.egn_data)
         stretched_data = (image - min_value) / (max_value - min_value)
 
         return (stretched_data * 255).astype(np.uint8)
-    
 
     def _egn_percentile_clip_stretch(self, image, min_percent, max_percent):
         """Apply percentile clipping to the sidescan EGN normalized image.
-        
+
         Args:
             min_percent (float): Lower percentile to clip (in 0-100%).
             max_percent (float): Upper percentile to clip (in 0-100%).
-            
+
         Returns:
             numpy.ndarray: Stretched data in the range [0, 1].
         """
         # note: this gets min - max percentile values from the whole image
-        min_value, max_value = np.percentile(
-            image, (min_percent, max_percent))
-        
+        min_value, max_value = np.percentile(image, (min_percent, max_percent))
+
         clipped_data = np.clip(image, min_value, max_value)
         stretched_data = (clipped_data - min_value) / (max_value - min_value)
 
         return (stretched_data * 255).astype(np.uint8)
-
 
     def _create_sonograms(self):
         image = self.image
 
         sonograms = {}
 
-        if self.config['remove_shadows']:
-            print('Removing shadow on side scan sonogram...')
-            
-        if self.config['water_column_present']:
-            print('Creating water column present...')
-            sonograms['sidescan_wcp'] = image
+        # Remove shadows
+        if self.config["remove_shadows"]:
+            pass
+            #print("Removing shadow on side scan sonogram...")
 
-        if self.config['water_column_removed']:
-            print('Creating water column removed...')
+        # Water column present
+        if self.config["water_column_present"]:
+            print("Creating water column present...")
+            sonograms["sidescan_wcp"] = image
+
+        # Water column removed
+        if self.config["water_column_removed"]:
+            print("Creating water column removed...")
             port_wcr, starboard_wcr = self._wcr(self._port(), self._starboard())
             image = np.concatenate((port_wcr, starboard_wcr), axis=1)
-            sonograms['sidescan_wcr'] = image
-        
+            sonograms["sidescan_wcr"] = image
+
+        # WCP and WCR
         for file_name, image in sonograms.items():
-            if self.config['egn']:
-                print(f'Applying EGN on {file_name}...')
+            # Apply EGN
+            if self.config["egn"]:
+                print(f"Applying EGN on {file_name}...")
                 image = self._egn(image)
 
-                if self.config['egn_stretch'] is not None:
-                    if self.config['egn_stretch'] == 1:
-                        print(f'Applying EGN min-max stretch on {file_name}...')
-                        pass
-                    elif self.config['egn_stretch'] == 2:
-                        print(f'Applying EGN percentile clip stretch on {file_name}...')
+                # Apply EGN stretch
+                if self.config["egn_stretch"] is not None:
+                    # Apply min-max stretch
+                    
+                    if self.config["egn_stretch"] == 1:
+                        print(f"Applying EGN min-max stretch on {file_name}...")
+                        pass # TODO
+
+                    # Apply percentile clip stretch
+                    elif self.config["egn_stretch"] == 2:
+                        print(f"Applying EGN percentile clip stretch on {file_name}...")
                         image = self._egn_percentile_clip_stretch(
                             image,
-                            self.config['egn_stretch_factor_min'],
-                            self.config['egn_stretch_factor_max'],
+                            self.config["egn_stretch_factor_min"],
+                            self.config["egn_stretch_factor_max"],
                         )
                     else:
                         # TODO: Raise error
                         # Stretch not found only hotdog
                         pass
 
-            sonograms[file_name] = image      
-        return sonograms
+            # Apply speed correction
+            print(f"Applying speed correction on {file_name}...")
+            image = self._apply_speed_correction(image)
 
+            # Save
+            sonograms[file_name] = image
+        
+        return sonograms
 
     def sonograms(self):
         "Export sonograms"
@@ -215,26 +231,24 @@ class SideScan:
         exporter = Exporter(self.config)
         exporter.export_sidescan_sonograms(sonograms)
 
-        return sonograms # to be optionally pass to georeferencing
-    
+        return sonograms  # to be optionally pass to georeferencing
 
     def georeference(self, sonograms):
         "export georeferenced"
         """
             args: output from sonograms (egned and/or stretched)
         """
-    
+
         exporter = Exporter(self.config)
 
         for file_name, image in sonograms.items():
-            georeferenced_image, bounds = self._create_georeference(image)
-
-            exporter.export_georeferenced_sidescan(
-                georeferenced_image,
-                file_name,
-                bounds
+            georeferenced_image, bounds = self._create_georeference(
+                image, self.config["resolution"]
             )
 
+            exporter.export_georeferenced_sidescan(
+                georeferenced_image, file_name, bounds
+            )
 
     def _create_georeference(self, image, resolution=1):
         """Georeference the sidescan image.
@@ -246,17 +260,21 @@ class SideScan:
             numpy.ndarray: Georeferenced sidescan image.
         """
         # Determine the coordinates of each pixel
-        distances = np.stack([
-            np.linspace(start, stop, len(f)) for start, stop, f in zip(
-            self.df['min_range'],
-            self.df['max_range'],
-            self.df['frames']
-        )])
-        sidescan_z = image # image is the intensities
-        sidescan_x = np.expand_dims(self.df['x'], axis=1) + distances * np.cos(
-            np.expand_dims(self.df['gps_heading'], axis=1))
+        distances = np.stack(
+            [
+                np.linspace(start, stop, len(f))
+                for start, stop, f in zip(
+                    self.df["min_range"], self.df["max_range"], self.df["frames"]
+                )
+            ]
+        )
+        sidescan_z = image  # image is the intensities
+        sidescan_x = np.expand_dims(self.df["x"], axis=1) + distances * np.cos(
+            np.expand_dims(self.df["gps_heading"], axis=1)
+        )
         sidescan_y = np.expand_dims(self.df["y"], axis=1) - distances * np.sin(
-            np.expand_dims(self.df['gps_heading'], axis=1))
+            np.expand_dims(self.df["gps_heading"], axis=1)
+        )
 
         # TODO: utils
         sidescan_long = x_to_longitude(sidescan_x)
@@ -267,7 +285,7 @@ class SideScan:
         min_y = np.min(sidescan_y)
 
         # Scale the coordinates to pixel values by resolution (meter per pixel)
-        #resolution = 1
+        # resolution = 1
         sidescan_x_scaled = ((sidescan_x - min_x) / resolution).astype(int)
         sidescan_y_scaled = ((sidescan_y - min_y) / resolution).astype(int)
 
@@ -294,7 +312,7 @@ class SideScan:
         image[sidescan_y_scaled, sidescan_x_scaled, 2] = sidescan_z
         image[sidescan_y_scaled, sidescan_x_scaled, 3] = 255
 
-        # since x y coordinate actually starts from bottom left, 
+        # since x y coordinate actually starts from bottom left,
         # while image application of pixel coordinates starts from top left,
         # we fix the orientation by flipping vertically
         image = np.flipud(image)
@@ -307,6 +325,51 @@ class SideScan:
 
         return image, (min_long, max_long, min_lat, max_lat)
 
+    def _add_cum_distance_travelled(self):
+        # Calculate the time elapsed between consecutive pings
+        self.df["time_elapsed"] = (
+            self.df["datetime"] - self.df["datetime"].shift()
+        ).fillna(pd.Timedelta(seconds=0))
+
+        # Convert the time elapsed to seconds
+        self.df["time_elapsed"] = self.df["time_elapsed"].dt.total_seconds()
+
+        # Calculate the cumulative sum of the time elapsed
+        self.df["cum_time_elapsed"] = self.df["time_elapsed"].cumsum()
+
+        # Calculate distance traveled between consecutive rows
+        self.df["dist_traveled"] = (
+            self.df["gps_speed"] * self.df["cum_time_elapsed"].diff()
+        )
+
+        # Fill NaN values in the first row with 0
+        self.df["dist_traveled"] = self.df["dist_traveled"].fillna(0)
+
+        # Calculate cumulative distance traveled
+        self.df["cum_dist_traveled"] = self.df["dist_traveled"].cumsum()
+
+    def _apply_speed_correction(self, image):
+        """Apply speed correction to the sidescan image by interpolating the
+        frames based on the distance traveled."""
+        cum_dist_traveled = self.df["cum_dist_traveled"].values
+        frames = image
+
+        # Define new evenly spaced distances
+        new_distances = np.linspace(
+            cum_dist_traveled.min(), cum_dist_traveled.max(), frames.shape[0]
+        )
+
+        # Interpolate along the row indices
+        indices = np.arange(frames.shape[0])
+        interpolated_indices = np.interp(new_distances, cum_dist_traveled, indices)
+
+        # Round and cast to integers to use as indices
+        interpolated_indices = np.round(interpolated_indices).astype(int)
+
+        # Interpolate the frames based on the interpolated indices
+        interpolated_frames = frames[interpolated_indices]
+
+        return interpolated_frames
 
     def __getattr__(self, attr):
         return getattr(self.image, attr)
